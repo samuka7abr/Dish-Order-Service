ce9d040838d12245b4fc1fede89e8899da191be8
samuka7abr <samuelabrao2006@gmail.com>
2025-04-02 17:06:54 -0300

    arrumando rotas

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,15 +3,11 @@ const { Router } = require("express");
 const usersRoutes = require("./users.routes");
 const sessionsRoutes = require("./sessions.routes");
 const dishesRoutes = require("./dishes.routes");
-const favoritesRoutes = require("./favorites.routes");
-const ordersRoutes = require("./orders.routes");
 
 const routes = Router();
 
 routes.use("/users", usersRoutes);
 routes.use("/sessions", sessionsRoutes);
 routes.use("/dishes", dishesRoutes);
-routes.use("/favorites", favoritesRoutes);
-routes.use("/orders", ordersRoutes);
 
 module.exports = routes;
