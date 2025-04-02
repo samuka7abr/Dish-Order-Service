85973f1bc4e8669e7de42bb865a7fe0ede8f45f3
samuka7abr <samuelabrao2006@gmail.com>
2025-04-02 17:01:47 -0300

    middleware de erro

--- src/server.js
+++ src/server.js
@@ -1,12 +1,30 @@
 require("dotenv/config");
 require("express-async-errors");
 const express = require("express");
+
 const routes = require("./routes");
+const AppError = require("./utils/AppError");
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
+
+app.use((error, request, response, next) => {
+  if (error instanceof AppError) {
+    return response.status(error.statusCode).json({
+      status: "error",
+      message: error.message
+    });
+  }
+
+  console.error(error);
+
+  return response.status(500).json({
+    status: "error",
+    message: "Internal server error"
+  });
+});
 
 const PORT = process.env.PORT || 3333;
 app.listen(PORT, () => console.log(`🚀 Server is running on port ${PORT}`));
